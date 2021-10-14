a457e748844c159ada0eb72b9178fc7dea7e2586
i <i@alexandermaru.com>
2021-10-14 04:32:07 +0000

    Generate employers controller

---
+++ app/controllers/employers_controller.rb
@@ -0,0 +1,25 @@
+class EmployersController < ApplicationController
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+    def employer_params
+      params.require(:employer).permit(:name, :email, :password, :password_confirmation)
+    end
+end
