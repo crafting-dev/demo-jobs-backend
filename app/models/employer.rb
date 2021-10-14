5228c15c00433bd0af246d62a2b63dfc3e83aa72
i <i@alexandermaru.com>
2021-10-14 03:57:17 +0000

    Enable bcrypt has_secure_password

--- app/models/employer.rb
+++ app/models/employer.rb
@@ -10,6 +10,7 @@ class Employer < ApplicationRecord
     validates :email, presence: true, length: { maximum: 255 },
                         format: { with: VALID_EMAIL_REGEX },
                         uniqueness: { case_sensitive: false }
+    has_secure_password
     validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
     validates :location, presence: true
 
