fd7622280c6159a83fc2acd4a664522223d8eef8
i <i@alexandermaru.com>
2021-10-14 04:56:34 +0000

    Generate sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :workers, except: [:index]
   resources :postings, except: [:edit, :update]
   resources :applications
+  resources :sessions, only: [:new, :create, :destroy]
 end
