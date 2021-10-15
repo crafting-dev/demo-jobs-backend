b60c70c688c49f95166dfc23adb659b3c85e1f15
i <i@alexandermaru.com>
2021-10-15 15:24:50 +0000

    Generate versioned Apps controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       resources :employers, only: %i[create show update destroy]
       resources :workers,   only: %i[create show update destroy]
       resources :postings,  only: %i[index create show destroy]
+      resources :apps,      only: %i[index create show destroy]
     end
   end
 end
