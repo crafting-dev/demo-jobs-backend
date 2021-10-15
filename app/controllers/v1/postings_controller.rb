ac0dc65e314b2d3744afaf2a9b4a47985acf3e13
i <i@alexandermaru.com>
2021-10-15 15:50:36 +0000

    Update Postings controller

--- app/controllers/v1/postings_controller.rb
+++ app/controllers/v1/postings_controller.rb
@@ -2,18 +2,55 @@
 
 module V1
   class PostingsController < ApplicationController
-    def index; end
+    before_action :set_posting, only: %i[show edit update destroy]
 
-    def show; end
+    # GET /postings
+    def index
+      @postings = Posting.all
+      respond_json(@postings)
+    end
 
-    def new; end
+    # GET /postings/:id
+    def show
+      respond_json(@posting)
+    end
 
-    def create; end
+    def new
+      @posting = Posting.new
+      respond_json(@posting)
+    end
 
-    def edit; end
+    # POST /postings
+    def create
+      @posting = Posting.create!(posting_params)
+      respond_json(@posting, :created)
+    end
 
-    def update; end
+    def edit
+      respond_json(@posting)
+    end
 
-    def destroy; end
+    # PUT /postings/:id
+    def update
+      @posting.update(posting_params)
+      head :no_content
+    end
+
+    # DELETE /postings/:id
+    def destroy
+      @posting.destroy
+      head :no_content
+    end
+
+    private
+
+    def posting_params
+      params.require(:posting).permit(:title, :description,
+                                      :employer_id, :hours, :status)
+    end
+
+    def set_posting
+      @posting = Posting.find(params[:id])
+    end
   end
 end
