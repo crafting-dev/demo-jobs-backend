a9e89ae00e0873fd2a9a5f8b986551d425856930
i <i@alexandermaru.com>
2021-10-15 15:53:07 +0000

    Update Apps controller

--- app/controllers/v1/apps_controller.rb
+++ app/controllers/v1/apps_controller.rb
@@ -2,18 +2,55 @@
 
 module V1
   class AppsController < ApplicationController
-    def index; end
+    before_action :set_app, only: %i[show edit update destroy]
 
-    def show; end
+    # GET /apps
+    def index
+      @apps = App.all
+      respond_json(@apps)
+    end
 
-    def new; end
+    # GET /apps/:id
+    def show
+      respond_json(@app)
+    end
 
-    def create; end
+    def new
+      @app = App.new
+      respond_json(@app)
+    end
 
-    def edit; end
+    # POST /apps
+    def create
+      @app = App.create!(app_params)
+      respond_json(@app, :created)
+    end
 
-    def update; end
+    def edit
+      respond_json(@app)
+    end
 
-    def destroy; end
+    # PUT /apps/:id
+    def update
+      @app.update(app_params)
+      head :no_content
+    end
+
+    # DELETE /apps/:id
+    def destroy
+      @app.destroy
+      head :no_content
+    end
+
+    private
+
+    def app_params
+      params.require(:app).permit(:posting_id, :worker_id,
+                                  :content, :status)
+    end
+
+    def set_app
+      @app = App.find(params[:id])
+    end
   end
 end
