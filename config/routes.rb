a0848a42ca591fc800d6304d3a7d41f39020789a
i <i@alexandermaru.com>
2021-10-15 15:32:55 +0000

    Generate sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :sessions, only: %i[create destroy]
+
   namespace :api do
     namespace :v1 do
       resources :employers, only: %i[create show update destroy]
