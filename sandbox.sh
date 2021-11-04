ae738b18830058c1eb9cbd7dfcca2607369ad09a
i <i@alexandermaru.com>
2021-11-04 16:27:27 +0000

    Run apt update before apt get

--- sandbox.sh
+++ sandbox.sh
@@ -4,14 +4,14 @@
 # Configure sandbox for quick workspace setup.
 # ============================================
 
+echo '== Update system =='
+sudo apt-get update
+
 echo '== Install libgmp3 =='
 sudo apt-get install libgmp3-dev -y
 
 echo '== Install mysql client =='
 sudo apt-get install mysql-client libmysqlclient-dev -y
-
-echo '== Update system =='
-sudo apt-get update
 
 echo '== Install latest rails =='
 gem install rails
